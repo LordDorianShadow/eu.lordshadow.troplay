0b9c0d7c341c58b44f9cc4dbffa4c7f0c5e76726
Mirco Martin <webmaster@mircomartin.de>
2021-02-02 22:58:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?PHP
+include 'sqlZugangsdaten.php';
+// Überprüfen ob über GET gesendet wurde.
+
+//if (isset($_GET['letsplay'])) {
+
+ // Den Zeichensatz über header() senden,
+ // sonst werden Umlaute ggf. nicht richtig angezeigt.
+ header('Content-Type: text/plain; charset=utf-8');
+//$letsplay=$_GET['letsplay'];
+
+ // Anweisung definieren
+
+function dbaseRequest(){
+$SQL_statment="SELECT * FROM test WHERE letsplay = the_medium"; 
+
+$resultset=mysql_query($SQL_statment);
+$data=mysql_fetch_array($resultset);
+	
+	echo ("Counter" + $data[counterAktuell]);
+	echo ("Counter" + $data[counterGesammt]);
+	
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>Suchanfrage an eine Datenbank über AJAX senden und empfangen</title>
+
+	 
+<body>	 
+  <script type="text/javascript">
+
+	  doccument.write(<?php dbaseRequest();?>)
+
+  </script>
+
+ </head>
+
+
+
+
+
+
+</body>
+</html>
