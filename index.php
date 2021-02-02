d928c0aff1b5e75caf215b9b2435d668d658d3da
Mirco Martin <webmaster@mircomartin.de>
2021-02-02 23:15:49 +0100

    mm

--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<?PHP
-
-
-include ("sqlZugangsdaten.php");
-// Überprüfen ob über GET gesendet wurde.
-
-//if (isset($_GET['letsplay'])) {
-
- // Den Zeichensatz über header() senden,
- // sonst werden Umlaute ggf. nicht richtig angezeigt.
- header('Content-Type: text/plain; charset=utf-8');
-//$letsplay=$_GET['letsplay'];
-
- // Anweisung definieren
-
-function dbaseRequest(){
-$SQL_statment="SELECT * FROM test WHERE letsplay = the_medium"; 
-
-$resultset=mysql_query($SQL_statment,$db);
-$data=mysql_fetch_array($resultset);
-	
-	echo ("Counter" + $data['counterAktuell']);
-	//echo ("Counter" + $data[counterGesammt]);
-	
-}
-?>
-
-
 <!DOCTYPE html>
 <html>
  <head>
