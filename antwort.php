aa762e466ce8335bed30f8ce0cba46dfa983f163
Mirco Martin <webmaster@mircomartin.de>
2021-02-02 22:45:26 +0100

    Update antwort.php

--- antwort.php
+++ antwort.php
@@ -8,7 +8,7 @@ if (isset($_GET['letsplay'])) {
  header('Content-Type: text/plain; charset=utf-8');
 $letsplay=$_GET['letsplay'];
 
-	echo($letsplay)
+	echo($letsplay);
  // Anweisung definieren
 
 $SQL_statment="SELECT * FROM test WHERE letsplay = ".$letsplay; 
