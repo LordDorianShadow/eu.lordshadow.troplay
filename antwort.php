788af76e63f1fdf9d8c9d907a835344be3da39ba
Mirco Martin <webmaster@mircomartin.de>
2021-02-02 22:40:15 +0100

    Update antwort.php

--- antwort.php
+++ antwort.php
@@ -1,25 +1,22 @@
 <?PHP
 include 'sqlZugangsdaten.php';
 // Überprüfen ob über GET gesendet wurde.
-if (isset($_GET['suchbegriff'])) {
+if (isset($_POST['letsplay'])) {
 
  // Den Zeichensatz über header() senden,
  // sonst werden Umlaute ggf. nicht richtig angezeigt.
  header('Content-Type: text/plain; charset=utf-8');
-$suchbegiff=$_GET['suchbegriff'];
-
- // Eine Verbindung zur Datenbank aufbauen
- $verbindung = new PDO("mysql:host='localhost:3306';dbname='letsplaystodeszaehler', 'troDeathcounter', 'troplayIsALooser!11!'");
+$letsplay=$_POST['letsplay'];
 
  // Anweisung definieren
- $kommando = $verbindung->query('SELECT `spalte1` FROM `test` WHERE (`spalte1` LIKE :suchbegriff OR`spalte2` LIKE :suchbegriff)');
 
-  // Den Platzhalter in der Anweisung mit dem Suchbegriff ersetzen
- $kommando->bindValue(':suchbegriff', '%' . $suchbegriff . '%');
+$SQL_statment="SELECT * FROM test WHERE letsplay = ".$letsplay; 
 
- // Die vorbereitete Anweisung ausführen
- $kommando->execute();
-
- // Datensätze holen
- $datensaetze = $kommando->fetchAll(PDO::FETCH_OBJ);
+$resultset=mysql_query($SQL_statment);
+$data=mysql_fetch_array($resultset);
+	
+	echo ("Counter" + $data[counterAktuell]);
+	echo ("Counter" + $data[counterGesammt]);
+	
+}
 ?>
